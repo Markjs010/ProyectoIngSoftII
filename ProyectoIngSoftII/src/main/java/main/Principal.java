c1fd1a3f8963110f82b5bc1e784f579eb8dec2ed
Marcos Daniel <mgomezv010@gmail.com>
2023-11-30 21:25:51 -0600

    Merge branch 'main' of https://github.com/Markjs010/ProyectoIngSoftII.git

--- ProyectoIngSoftII/src/main/java/main/Principal.java
+++ ProyectoIngSoftII/src/main/java/main/Principal.java
@@ -1,12 +1,37 @@
 package main;
 
-import dbconn.DBConn;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
 
 public class Principal {
 
 	public static void main(String[] args) {
-		DBConn connectTest = new DBConn();
+			Connection conn = getDBConnection();
+			if(conn != null) {
+				
+			}
+				
 
 	}
+	
+	public static Connection getDBConnection() {
+		 String url          = "jdbc:derby:BaseDatosProyecto;create=true";
+		 try
+	        {
+	            Connection conn = DriverManager.getConnection(url);
+	            System.out.println("Conectado ... "+ conn);
+	            return conn;
+	        }
+	        catch (SQLException e)
+	        {
+	              JOptionPane.showMessageDialog(null, "FallÃ³ la conexiÃ³n " + e);  
+	              return null;
+	        }       
+		} //FIN GET_CONN
+	
 
 }
