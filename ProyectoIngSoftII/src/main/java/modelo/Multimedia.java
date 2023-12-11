d81055adda1ff19f8b02b4d1e614e547045ee69f
Jsebastian96 <89179453+Jsebastian96@users.noreply.github.com>
2023-12-11 01:58:26 -0500

    Add files via upload

--- ProyectoIngSoftII/src/main/java/modelo/Multimedia.java
+++ ProyectoIngSoftII/src/main/java/modelo/Multimedia.java
@@ -3,5 +3,25 @@ package modelo;
 
 public class Multimedia implements Data{
 	
+	private String informacion;
 
+	public Multimedia(String informacion) {
+		super();
+		this.informacion = informacion;
+	}
+
+	public Multimedia() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getInformacion() {
+		return informacion;
+	}
+
+	public void setInformacion(String informacion) {
+		this.informacion = informacion;
+	}
+	
+	
 }
